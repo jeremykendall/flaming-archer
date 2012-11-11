786a7f36235e92aec967b369938f9db9dee154b1
Jeremy Kendall <jeremy@jeremykendall.net>
2012-11-11 11:09:14 -0600

    Vagrant files and manifest

---
+++ vagrant/manifests/base.pp
@@ -0,0 +1,36 @@
+# base.pp
+stage { 'pre': before  => Stage['main'] }
+
+class base {
+    group { 'puppet':
+        ensure => present,
+    }
+
+    user { 'vagrant':
+        groups => [
+            'sudo'
+        ]
+    }
+
+    exec { 'apt-get -y update':
+        alias  => 'aptupdate',
+        path   => '/usr/bin',
+        user   => 'root',
+    }
+
+}
+
+class {'base': stage => pre}
+
+class {'php5':}
+php5::pkg { [
+    'php5-intl',
+    'php-apc'
+]:}
+
+class {'apache2':}
+apache2::vhost { 'flaming-archer.dev':
+    port            => 80,
+    docroot         => '/home/vagrant/sites/dev.flaming-archer/public',
+    configfile_name => 'dev.flaming-archer'
+}
