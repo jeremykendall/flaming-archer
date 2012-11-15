7dd6ec23d5812e8ff3b31b908e5e53299fe7d605
Jeremy Kendall <jeremy@jeremykendall.net>
2012-11-14 19:26:40 -0600

    Adds d() and dd() and some require_once calls

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,16 @@
 <?php
 
-require '../vendor/autoload.php';
+require_once realpath(__DIR__ . '/../vendor/autoload.php');
+require_once 'PHPUnit/Autoload.php';
+require_once 'CommonDbTestCase.php';
+
+define('APPLICATION_PATH', realpath(__DIR__ . '/..'));
+
+function d($expression) {
+    var_dump($expression);
+}
+
+function dd($expression) {
+    d($expression);
+    die();
+}
