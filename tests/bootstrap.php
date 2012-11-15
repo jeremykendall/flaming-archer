5bfd2dc89c44dfca757b329dcc3cd570cd161546
Jeremy Kendall <jeremy@jeremykendall.net>
2012-11-15 08:04:34 -0600

    Adds error reporting settings and timezone setting

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+date_default_timezone_set('America/Chicago');
+
 require_once realpath(__DIR__ . '/../vendor/autoload.php');
 require_once 'PHPUnit/Autoload.php';
 require_once 'CommonDbTestCase.php';
