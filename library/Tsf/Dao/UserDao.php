b27a2dd3ffbf9cb9c2480a62cd70b247d1730bcc
Jeremy Kendall <jeremy@jeremykendall.net>
2012-11-16 22:07:24 -0600

    Refactors auth adapter to remove PDO, use a Dao

---
+++ library/Tsf/Dao/UserDao.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tsf\Dao;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserDao
+ *
+ * @author jkendall
+ */
+class UserDao
+{
+
+    /**
+     * @var \PDO
+     */
+    protected $db;
+
+    /**
+     * Public constructor
+     *
+     * @param \PDO Database connection
+     */
+    public function __construct(\PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * Finds user by email address
+     *
+     * @param  string $email User's email address
+     * @return array  User record
+     */
+    public function findByEmail($email)
+    {
+        $sql = 'SELECT * FROM users WHERE email = :email';
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':email', $email, \PDO::PARAM_STR);
+        $stmt->execute();
+        $user = $stmt->fetch();
+
+        return $user;
+    }
+
+}
