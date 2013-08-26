33590d6a472c97eb295bf0f98fbcf65e31b6f4c7
Jeremy Kendall <jkendall@opensky.com>
2013-08-26 17:26:25 -0500

    Changes require_once to require for autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 
 date_default_timezone_set('America/Chicago');
 
-$loader = require_once realpath(__DIR__ . '/../vendor/autoload.php');
+$loader = require realpath(__DIR__ . '/../vendor/autoload.php');
 $loader->add('FA\\', __DIR__);
 
 define('APPLICATION_PATH', realpath(__DIR__ . '/..'));
