bf229fdf97534396940202fefb5c6a95dfe585a5
Jeremy Kendall <jkendall@opensky.com>
2013-08-26 00:27:48 -0500

    Reformats schema.sql

--- scripts/sql/schema.sql
+++ scripts/sql/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS `images` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `day` INT unsigned NOT NULL,
-  `photo_id` INT unsigned NOT NULL,
-  `posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   UNIQUE (`day`),
-   UNIQUE (`photo_id`)
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `day` INT unsigned NOT NULL,
+    `photo_id` INT unsigned NOT NULL,
+    `posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (`day`),
+    UNIQUE (`photo_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `email` TEXT NOT NULL,
-  `password_hash` TEXT NOT NULL,
-  `last_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-   UNIQUE (`email`)
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `email` TEXT NOT NULL,
+    `password_hash` TEXT NOT NULL,
+    `last_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (`email`)
 );
