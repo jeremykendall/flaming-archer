e93ef43c820330b0e853f0a44fc8ef3b4c03798c
Jeremy Kendall <jeremy@jeremykendall.net>
2013-08-25 20:31:25 -0500

    Adds unique columns on images.photo_id and users.email.

--- scripts/sql/schema.sql
+++ scripts/sql/schema.sql
@@ -2,12 +2,14 @@ CREATE TABLE IF NOT EXISTS `images` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `day` INT unsigned NOT NULL,
   `photo_id` INT unsigned NOT NULL,
-  `posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   UNIQUE (`photo_id`) ON CONFLICT FAIL
 );
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `email` TEXT NOT NULL,
   `password_hash` TEXT NOT NULL,
-  `last_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `last_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+   UNIQUE (`email`) ON CONFLICT FAIL
 );
