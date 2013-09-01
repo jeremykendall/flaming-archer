404896c71f9638635a412a06781fadea15894209
Jeremy Kendall <jeremy@jeremykendall.net>
2013-09-01 15:36:58 -0500

    Implements user management stuff

--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-    error_reporting(-1);
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
 require '../vendor/autoload.php';
 
 $config = require_once __DIR__ . '/../config.php';
@@ -18,6 +15,8 @@ use FA\Pagination;
 use FA\Service\FlickrService;
 use FA\Service\FlickrServiceCache;
 use FA\Service\ImageService;
+use FA\Service\UserService;
+use Slim\Log;
 use Slim\Middleware\SessionCookie;
 use Slim\Slim;
 use Slim\Views\Twig;
@@ -40,7 +39,7 @@ try {
 $pagination = new Pagination();
 
 $userDao = new UserDao($db);
-$authAdapter = new DbAdapter($userDao, new Phpass\Hash());
+$authAdapter = new DbAdapter($userDao);
 
 $cache = StorageFactory::factory($config['cache']);
 $flickrService = new FlickrService($config['flickr.api.key']);
@@ -52,26 +51,36 @@ $service = new ImageService(new ImageDao($db), $flickrServiceCache);
 $app = new Slim($config['slim']);
 
 // Dev mode settings
-$app->configureMode('development', function() {
+$app->configureMode('development', function() use ($app, $config) {
     error_reporting(-1);
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
+
+    $app->config(array(
+        'log.enabled' => true,
+        'log.level' => Log::DEBUG,
+    ));
+
+    $config['twig']['debug'] = true;
 });
 
 $auth = new AuthenticationService();
 $storage = new EncryptedCookie($app);
 $auth->setStorage($storage);
+$auth->setAdapter($authAdapter);
+
+$userService = new UserService($userDao, $auth);
 
 // Add Middleware
 $app->add(new Profile($config));
 $app->add(new Navigation($auth));
 $app->add(new Authentication($auth, $config));
-$app->add(new SessionCookie($config['cookies']));
+$app->add(new SessionCookie($config['session_cookies']));
 
 // Prepare view
 $app->view(new Twig());
 $app->view->parserOptions = $config['twig'];
-$app->view->parserExtensions = array(new TwigExtension());
+$app->view->parserExtensions = array(new TwigExtension(), new Twig_Extension_Debug());
 
 // Define routes
 $app->get('/', function ($page = 1) use ($app, $service, $pagination) {
@@ -143,6 +152,33 @@ $app->get('/admin/settings', function () use ($app) {
     $app->render('admin/settings.html', array('user' => $user));
 });
 
+$app->post('/admin/user', function () use ($app, $userService) {
+    $user = json_decode($app->getCookie('identity'), true);
+    $params = $app->request()->post();
+
+    $email = filter_var($params['email'], FILTER_SANITIZE_EMAIL);
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) && ($email != $user['email'])) {
+        $userService->updateEmail($user, $email);
+        $app->log->info(sprintf('Email changed from %s to %s', $user['email'], $email));
+        $app->flash('emailSuccess', 'Your email is now ' . $email);
+    }
+
+    if ($params['form-type'] == 'change-password' && $params['password']) {
+        $app->log->info(sprintf('About to change password for %s', $user['email']));
+        try {
+            $result = $userService->changePassword($user['email'], $params['password'], $params['new-password'], $params['confirm-password']);
+            $app->log->info(sprintf('Password changed for %s', $user['email']));
+            $app->flash('passwordSuccess', 'Password changed!');
+        } catch (\Exception $e) {
+            $app->log->error(sprintf('Error changing password: %s', $e->getMessage()));
+            $app->flash('passwordError', $e->getMessage());
+        }
+    }
+
+    $app->redirect('/admin/settings');
+});
+
 $app->post('/admin/add-photo', function() use ($app, $service, $cache) {
     $data = $app->request()->post();
     $service->save($data);
@@ -157,17 +193,14 @@ $app->post('/admin/delete-photo', function() use ($app, $service, $cache) {
     $app->redirect('/admin');
 });
 
-$app->map('/login', function() use ($app, $auth, $authAdapter) {
+$app->map('/login', function() use ($app, $userService) {
 
     $email = null;
 
     if ($app->request()->isPost()) {
 
         $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-
-        $authAdapter->setCredentials($email, $_POST['password']);
-        $auth->setAdapter($authAdapter);
-        $result = $auth->authenticate();
+        $result = $userService->authenticate($email, $_POST['password']);
 
         if (!$result->isValid()) {
             $messages = $result->getMessages();
@@ -180,21 +213,10 @@ $app->map('/login', function() use ($app, $auth, $authAdapter) {
     $app->render('login.html', array('email' => $email));
 })->via('GET', 'POST');
 
-$app->get('/logout', function() use ($app, $auth) {
-    $auth->clearIdentity();
+$app->get('/logout', function() use ($app, $userService) {
+    $userService->clearIdentity();
     $app->redirect('/');
 });
 
 // Run app
 $app->run();
-
-function d($expression)
-{
-    var_dump($expression);
-}
-
-function dd($expression)
-{
-    d($expression);
-    die();
-}
