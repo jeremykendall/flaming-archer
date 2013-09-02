bbd1c898ecdf7e39fc525bf8d95830b74fd820a9
Jeremy Kendall <jeremy@jeremykendall.net>
2013-09-02 09:09:54 -0500

    Moves and reorganizes distribution config file

--- config.dist.php
+++ config.dist.php
@@ -1,7 +1,31 @@
 <?php
+/**
+ * Flaming Archer
+ *
+ * @link      http://github.com/jeremykendall/flaming-archer for the canonical source repository
+ * @copyright Copyright (c) 2012 Jeremy Kendall (http://about.me/jeremykendall)
+ * @license   http://github.com/jeremykendall/flaming-archer/blob/master/LICENSE MIT License
+ */
 
-// SQLite database file
-$sqlite = __DIR__ . '/db/flaming-archer.db';
+/**
+ * EDIT THE ITEMS IN THE $userConfig ARRAY TO COMPLETE APPLICATION CONFIGURATION
+ *
+ * Remember, you must have a Flickr API key in order to use Flaming Archer
+ */
+$userConfig = array(
+    'flickr.api.key' => '@@@ Your Flickr API key @@@',
+    // Change these settings to whatever you like
+    'profile' => array(
+        'brand' => 'Flaming Archer',
+        'site_name' => '365 Days of Photography',
+        'flickr_username' => '@@@ Your Flickr username @@@',
+        'photographer' => '@@@ Your name @@@',
+        'tagline' => "@@@ Some clever tagline @@@",
+        'external_url' => '@@@ Website, Flickr profile, blog, etc. @@@',
+    ),
+    // Change this to a random-ish string. It's used for encrypting cookies.
+    'cookies.secret_key' => 'CHANGE_ME',
+);
 
 // Slim configuration
 $slim = array(
@@ -15,23 +39,17 @@ $slim = array(
         )
     ),
     // Global, not SessionCookie, cookie settings
+    'cookies.secret_key' => $userConfig['cookies.secret_key'],
     'cookies.encrypt' => false, // must be false until https://github.com/codeguy/Slim/pull/606 is merged
-    'cookies.secret_key' => 'CHANGE_ME',
     'cookies.cipher' => MCRYPT_RIJNDAEL_256,
     'cookies.cipher_mode' => MCRYPT_MODE_CBC,
 );
 
+// SQLite database file
+$sqlite = __DIR__ . '/db/flaming-archer.db';
+
 $config = array(
     'slim' => $slim,
-    'profile' => array(
-        'brand' => 'Flaming Archer',
-        'site_name' => 'SITE NAME',
-        'flickr_username' => 'YOUR FLICKR USERNAME',
-        'photographer' => 'YOUR NAME',
-        'tagline' => "TAGLINE",
-        'external_url' => 'BLOG, FLICKR PROFILE, WHATEVER',
-    ),
-    'flickr.api.key' => 'YOUR FLICKR API KEY',
     'twig' => array(
         'environment' => array(
             'charset' => 'utf-8',
@@ -48,7 +66,7 @@ $config = array(
         'secure' => false,
         'httponly' => false,
         'name' => 'slim_session',
-        'secret' => $slim['cookies.secret_key'],
+        'secret' => $userConfig['cookies.secret_key'],
         'cipher' => $slim['cookies.cipher'],
         'cipher_mode' => $slim['cookies.cipher_mode'],
     ),
@@ -88,4 +106,4 @@ $config = array(
     ),
 );
 
-return $config;
+return array_merge($userConfig, $config);
