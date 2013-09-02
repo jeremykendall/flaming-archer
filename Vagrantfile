c06173af69b9185038e1399f7199f95be0f19913
Jeremy Kendall <jeremy@jeremykendall.net>
2013-09-01 23:12:07 -0500

    Adds puphpet Vagrant files

---
+++ Vagrantfile
@@ -0,0 +1,32 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "precise64"
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+
+  config.vm.network :private_network, ip: "192.168.33.10"
+    config.ssh.forward_agent = true
+
+  config.vm.provider :virtualbox do |v|
+    v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    v.customize ["modifyvm", :id, "--memory", 1024]
+    v.customize ["modifyvm", :id, "--name", "flaming-archer.dev"]
+  end
+  
+  config.vm.synced_folder "./", "/var/www", id: "vagrant-root",
+    :owner => "vagrant",
+    :group => "www-data",
+    :extra => "dmode=775,fmode=664"
+
+  config.vm.provision :shell, :inline =>
+    "if [[ ! -f /apt-get-run ]]; then sudo apt-get update && sudo touch /apt-get-run; fi"
+
+  config.vm.provision :shell, :inline => 'echo -e "mysql_root_password=controluser_password=awesome" > /etc/phpmyadmin.facts;'
+
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = "manifests"
+    puppet.module_path = "modules"
+    puppet.options = ['--verbose']
+  end
+end
---
+++ manifests/default.pp
@@ -0,0 +1,163 @@
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+    apt::key { '4F4EA0AAE5267A6C': }
+
+apt::ppa { 'ppa:ondrej/php5-oldstable':
+  require => Apt::Key['4F4EA0AAE5267A6C']
+}
+
+class { 'puphpet::dotfiles': }
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core'
+  ]:
+  ensure  => 'installed',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+apache::module { 'rewrite': }
+
+apache::vhost { 'flaming-archer.dev':
+  server_name   => 'flaming-archer.dev',
+  serveraliases => [
+],
+  docroot       => '/var/www/public/',
+  port          => '80',
+  env_variables => [
+    'APP_ENV develop'
+  ],
+  priority      => '1',
+}
+
+class { 'php':
+  service             => 'apache',
+  service_autorestart => false,
+  module_prefix       => '',
+}
+
+php::module { 'php5-cli': }
+php::module { 'php5-curl': }
+php::module { 'php5-intl': }
+php::module { 'php5-mcrypt': }
+php::module { 'php5-sqlite': }
+
+class { 'php::devel':
+  require => Class['php'],
+}
+
+class { 'php::pear':
+  require => Class['php'],
+}
+
+$xhprofPath = '/var/www/xhprof'
+
+php::pecl::module { 'xhprof':
+  use_package     => false,
+  preferred_state => 'beta',
+}
+
+if !defined(Package['git-core']) {
+  package { 'git-core' : }
+}
+
+vcsrepo { $xhprofPath:
+  ensure   => present,
+  provider => git,
+  source   => 'https://github.com/facebook/xhprof.git',
+  require  => Package['git-core']
+}
+
+file { "${xhprofPath}/xhprof_html":
+  ensure  => 'directory',
+  owner   => 'vagrant',
+  group   => 'vagrant',
+  mode    => '0775',
+  require => Vcsrepo[$xhprofPath]
+}
+
+composer::run { 'xhprof-composer-run':
+  path    => $xhprofPath,
+  require => [
+    Class['composer'],
+    File["${xhprofPath}/xhprof_html"]
+  ]
+}
+
+composer::run { 'flaming-archer-run':
+  path    => '/var/www',
+  require => [
+    Class['composer'],
+  ]
+}
+
+apache::vhost { 'xhprof':
+  server_name => 'xhprof',
+  docroot     => "${xhprofPath}/xhprof_html",
+  port        => 80,
+  priority    => '1',
+  require     => [
+    Php::Pecl::Module['xhprof'],
+    File["${xhprofPath}/xhprof_html"]
+  ]
+}
+
+class { 'xdebug':
+  service => 'apache',
+}
+
+class { 'composer':
+  require => Package['php5', 'curl'],
+}
+
+puphpet::ini { 'xdebug':
+  value   => [
+    'xdebug.default_enable = 1',
+    'xdebug.remote_autostart = 0',
+    'xdebug.remote_connect_back = 1',
+    'xdebug.remote_enable = 1',
+    'xdebug.remote_handler = "dbgp"',
+    'xdebug.remote_port = 9000'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_xdebug.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'php':
+  value   => [
+    'date.timezone = "America/Chicago"'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_php.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'custom':
+  value   => [
+    'display_errors = On',
+    'error_reporting = -1',
+    'display_startup_errors = 1'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_custom.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
