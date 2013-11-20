762f51e94c114275a3eb5785fc653c951ccad7b4
Jeremy Kendall <jeremy@jeremykendall.net>
2013-11-20 14:32:33 -0600

    Adds service to notify pubsubhubbub when feed is updated

---
+++ src/FA/Service/PubSubNotifier.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace FA\Service;
+
+use Guzzle\Http\ClientInterface;
+use Guzzle\Http\Exception\BadResponseException;
+use Psr\Log\LoggerInterface;
+
+class PubSubNotifier
+{
+    protected $client;
+
+    protected $logger;
+
+    protected $hubUrl;
+
+    /**
+     * Public constructor
+     *
+     * @param Client $client Http client
+     * @param LoggerInterface PSR Logger Interface
+     * @param string $hubUrl  Url of pubsubhubbub hub to notify
+     */
+    public function __construct(ClientInterface $client, LoggerInterface $logger, $hubUrl)
+    {
+        $this->client = $client;
+        $this->logger = $logger;
+        $this->hubUrl = $hubUrl;
+    }
+
+    /**
+     * Notify PubSubHubbub Hub
+     *
+     * @param string $feedUrl URL of the feed that has been updated
+     * @param string $mode    Should be publish to notify hub of feed update
+     */
+    public function notify($feedUrl, $mode = 'publish')
+    {
+        $request = $this->client->post($this->hubUrl, array(), array(
+            'hub.mode' => $mode,
+            'hub.url' => $feedUrl,
+        ));
+
+        try {
+            $response = $request->send();
+            $this->logger->debug('Notification request send to pubsubhubbub');
+            $this->logger->debug($request);
+            $this->logger->debug($response);
+        } catch (BadResponseException $e) {
+            $this->logger->error(
+                sprintf('Bad response from pubsubhubbub: %s', $e->getMessage())
+            );
+        }
+    }
+}
---
+++ tests/FA/Tests/Service/PubSubNotifierTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace FA\Tests\Service;
+
+use FA\Service\PubSubNotifier;
+use Guzzle\Http\Exception\BadResponseException;
+use Guzzle\Http\Message\Response;
+
+class PubSubNotifierTest extends \PHPUnit_Framework_TestCase
+{
+    protected $client;
+    protected $request;
+    protected $logger;
+    protected $hubUrl;
+    protected $feedUrl;
+    protected $notifier;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->client = $this->getMockBuilder('Guzzle\Http\ClientInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->request = $this->getMockBuilder('Guzzle\Http\Message\RequestInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->logger = $this->getMockBuilder('Psr\Log\LoggerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->feedUrl = 'http://example.com/feed';
+        $this->hubUrl = 'http://pubsub.example.com';
+
+        $this->notifier = new PubSubNotifier($this->client, $this->logger, $this->hubUrl);
+    }
+
+    public function testNotify()
+    {
+        $this->client->expects($this->once())
+            ->method('post')
+            ->with($this->hubUrl, array(), array(
+                'hub.mode' => 'publish',
+                'hub.url' => $this->feedUrl,
+            ))
+            ->will($this->returnValue($this->request));
+
+        $response = new Response(200);
+
+        $this->request->expects($this->once())
+            ->method('send')
+            ->will($this->returnValue($response));
+
+        $this->logger->expects($this->once())
+            ->method('debug')
+            ->with('Notification request send to pubsubhubbub');
+
+        $this->notifier->notify($this->feedUrl);
+    }
+
+    public function testNotifyBadResponseFromHub()
+    {
+        $this->client->expects($this->once())
+            ->method('post')
+            ->with($this->hubUrl, array(), array(
+                'hub.mode' => 'publish',
+                'hub.url' => $this->feedUrl,
+            ))
+            ->will($this->returnValue($this->request));
+
+        $response = new Response(500);
+        $e = BadResponseException::factory($this->request, $response);
+
+        $this->request->expects($this->once())
+            ->method('send')
+            ->will($this->throwException($e));
+
+        $this->logger->expects($this->once())
+            ->method('error')
+            ->with(sprintf('Bad response from pubsubhubbub: %s', $e->getMessage()));
+
+        $this->notifier->notify($this->feedUrl);
+    }
+}
